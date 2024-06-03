a3cc4eb315326630342a89b2fd971e00a2213b03
Pistol Alexandru Daniel <alexey9021@gmail.com>
2024-06-03 23:55:17 +0300

    Fix for navbar overflowing off screen

--- packages/client/src/components/NavbarSection/NavbarSection.tsx
+++ packages/client/src/components/NavbarSection/NavbarSection.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 function NavbarSection({ children }: Props) {
   return (
-    <div className="flex aspect-square h-full w-auto items-center justify-center border-l border-[#494E6E] first-of-type:border-none md:h-auto md:w-full md:border-l-0 md:border-t md:first-of-type:border-none">
+    <div className="flex aspect-square w-20 items-center justify-center border-l border-[#494E6E] first-of-type:border-none md:h-auto md:w-full md:border-l-0 md:border-t md:first-of-type:border-none">
       {children}
     </div>
   );
