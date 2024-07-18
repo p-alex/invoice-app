aef89b8a8c30f42b1c88128e9542beecc341b668
Pistol Alexandru Daniel <alexey9021@gmail.com>
2024-07-18 13:18:05 +0300

    Added Invoice and InvoiceItem entities

--- packages/client/src/entities/Invoice.ts
+++ packages/client/src/entities/Invoice.ts
@@ -1,16 +1,22 @@
-export interface IInvoice {
-  id: string;
-  from_streetAddress: string;
-  from_city: string;
-  from_postCode: string;
-  to_clientName: string;
-  to_clientEmail: string;
-  to_streetAddress: string;
-  to_city: string;
-  to_postCode: string;
-  to_country: string;
-  invoiceDate: Date;
-  paymentTerms: PaymentTerms;
-}
+import { z } from "zod";
+import { invoiceItemSchema } from "./InvoiceItem";
 
-type PaymentTerms = "Net 1 Day" | "Net 7 Days" | "Net 14 Days" | "Net 30 Days";
+export const invoiceSchema = z.object({
+  id: z.string().min(1, "Can't be blank"),
+  from_streetAddress: z.string().min(1, "Can't be blank"),
+  from_city: z.string().min(1, "Can't be blank"),
+  from_postCode: z.string().min(1, "Can't be blank"),
+  from_country: z.string().min(1, "Can't be blank"),
+  to_clientName: z.string().min(1, "Can't be blank"),
+  to_clientEmail: z.string().min(1, "Can't be blank").email(),
+  to_streetAddress: z.string().min(1, "Can't be blank"),
+  to_city: z.string().min(1, "Can't be blank"),
+  to_postCode: z.string().min(1, "Can't be blank"),
+  to_country: z.string().min(1, "Can't be blank"),
+  invoiceDate: z.string().min(1, "Can't be blank"),
+  paymentTerms: z.enum(["Net 1 Day", "Net 7 Days", "Net 14 Days", "Net 30 Days"]),
+  projectDescription: z.string().min(1, "Can't be blank"),
+  invoiceItemList: z.array(invoiceItemSchema).min(1, "No invoice item added"),
+});
+
+export type InvoiceType = z.TypeOf<typeof invoiceSchema>;
--- packages/client/src/entities/InvoiceItem.ts
+++ packages/client/src/entities/InvoiceItem.ts
@@ -1,7 +1,11 @@
-export interface IInvoiceItem {
-  id: string;
-  invoiceId: string;
-  name: string;
-  quantity: number;
-  price: number;
-}
+import { z } from "zod";
+
+export const invoiceItemSchema = z.object({
+  id: z.string().min(1, "Can't be blank"),
+  invoiceId: z.string().min(1, "Can't be blank"),
+  name: z.string().min(1, "The name field of all invoice items has to be populated"),
+  quantity: z.number().min(1, "The quantity field of all invoice items must be at least 1"),
+  price: z.number(),
+});
+
+export type InvoiceItemType = z.TypeOf<typeof invoiceItemSchema>;
