1c2c26d409e96c04449be00d307f8285ac484ad4
Alex Daniel <alexey9021@gmail.com>
2024-09-12 13:00:42 +0300

    Replace div backdrop with Backdrop component in SideModal

--- packages/client/src/components/SideModal/SideModal.tsx
+++ packages/client/src/components/SideModal/SideModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CloseIcon } from "../../svgs";
+import Backdrop from "../Backdrop/Backdrop";
 
 interface Props {
   children: React.ReactNode;
@@ -11,11 +12,7 @@ interface Props {
 const SideModal = ({ children, title, handleCloseModal, closeButtonRef }: Props) => {
   return (
     <div className="fixed left-0 top-0 z-10">
-      <div
-        className="fixed left-0 top-0 z-0 h-full w-full bg-[rgba(0,0,0,0.7)]"
-        onClick={handleCloseModal}
-        data-testid={"sideModalBackdrop"}
-      ></div>
+      <Backdrop func={handleCloseModal} data-testid={"sideModalBackdrop"} />
       <div className="fixed left-0 top-0 z-10 mt-20 h-[calc(100vh-80px)] w-full max-w-[779px] overflow-y-scroll rounded-br-3xl rounded-tr-3xl bg-modalBgLt px-6 py-8 max-[616px]:rounded-br-none max-[616px]:rounded-tr-none md:mt-0 md:h-screen md:pl-[159px] md:pr-14 dark:bg-modalBgDT">
         <header className="mb-12 flex w-full items-center justify-between">
           <h2 className="text-2xl font-bold text-textLT dark:text-textDT">{title}</h2>
